1ce6107e63611c724308764288643bf24c404ea4
chrisg220 <chrisg220@hotmail.com>
2013-03-07 11:59:00 -0800

    Generate bus-trip-planner application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BusTripPlanner::Application.config.secret_token = '03fe914cfe54670050fb02d3ec3f5cdc17972331f5a126bbce4f6c184025089c0834c597d5b39acc79710329116a96ef0fc730efd92718bf8d7ff434d1cfca6b'
