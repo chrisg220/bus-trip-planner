a59d6ae788089dd30441e172537d80ca89e73308
ch <cameron.hill@gmail.com>
2013-03-19 10:37:31 -0700

    Add buttons and feature to Edit and Delete trips. No test specs written yet

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -8,7 +8,8 @@ require 'open-uri'
 require 'json'
 
 class TripsController < ApplicationController
-  before_filter :authenticate_user!, :only => [:edit, :update, :delete, :save]
+  before_filter :find_trip, :only => [:show, :edit, :update, :destroy, :save]
+  before_filter :authenticate_user!, :only => [:edit, :update, :destroy, :save]
 
   def index
     # eventually this should only be trips that current_user has access to
@@ -21,7 +22,6 @@ class TripsController < ApplicationController
   end
 
   def create
-
     @trip = Trip.new(params[:trip])
     origin = params[:trip][:origin_name]
     destination = params[:trip][:destination_name]
@@ -63,14 +63,36 @@ class TripsController < ApplicationController
   end
 
   def show
-    @trip = Trip.find(params[:id])
     @json = JSON.parse(@trip.raw_response)
     # this action will trigger SAVING the trip for the current user.
     # in create, the trip is saved but not assigned a user
     # in order to edit, delete, etc, the trip must first be saved
   end
 
+  def edit
+  end
+
+  def update
+    if @trip.update_parameters(params[:trip])
+      flash[:notice] = "Trip start/end updated."
+      redirect_to trip_path(@trip)
+    else
+      flash[:error] = "Trip was not updated."
+      render "edit"
+    end
+  end
+
+  def destroy
+    @trip.destroy
+    flash[:error] = "Trip was deleted!"
+    redirect_to trips_path
+  end
+
   private
+
+  def find_trip
+    @trip = Trip.find(params[:id])
+  end
 
   def authenticate_user!
     # this is identical to ticketee
