6d1d01c6f889d1c5a8a47e6773217dc37cd62874
zaithottakath <zaithottakath@gmail.com>
2013-03-25 23:27:56 -0700

    Added a parsing method from gmaps_api to an Array of Hashes that is more easily enumerated over and saved to response. Remaining values in attr_accessor need to be found within the gmaps hash.

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,3 +1,26 @@
 class Route < ActiveRecord::Base
   attr_accessible :duration, :end_time, :response, :snapshot, :start_time, :trip_id
+  attr_accessor :route
+  belongs_to :trip
+
+  def parse_gmaps
+    require 'json'
+    legs = Array.new
+    self.route["legs"].each do |leg|
+      leg["steps"].each_with_index do |step, l|
+        legs[l] = Hash.new
+        legs[l]["travel_mode"]= step["travel_mode"]
+        legs[l]["html_instructions"]  = step["html_instructions"]
+        if step["transit_details"]
+          legs[l]["line"] = step["transit_details"]["line"]["short_name"]
+          legs[l]["vehicle_name"] = step["transit_details"]["line"]["vehicle"]["name"]
+          legs[l]["departure_stop"]= step["transit_details"]["departure_stop"]["name"]
+          legs[l]["arrival_stop"] = step["transit_details"]["arrival_stop"]["name"]
+        end
+      end
+    end
+    #this returns a string
+    self.response = JSON.generate(legs)
+    #JSON.parse returns the data structure exactly
+  end
 end
